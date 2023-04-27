6bf469963ff9263ec13b2301dcc9e605cab7ee6c
Ishan Jain <ishanjain1502@gmail.com>
2023-04-27 18:58:25 +0530

    buy ico -fixed

--- controllers/investor/buyIco.js
+++ controllers/investor/buyIco.js
@@ -1,6 +1,7 @@
 const Ico = require('../../models/ico_model');
 const Investor = require('../../models/investor_model');
 const Txn = require('../../models/transaction_model');
+const { v4: uuidv4 } = require('uuid');
 
 /**
  * Buy ico
@@ -17,8 +18,6 @@ const buyIco = async(req,res) => {
     try{
         const ifIco=await Ico.find({id: ico_id});
 
-        console.log(ifIco);
-        console.log(ifIco[0].total_ico, ifIco[0].sold_ico)
         const ico_left=ifIco[0].total_ico-ifIco[0].sold_ico;
         // return  res.send('hello beta')
         if(ifIco){
@@ -36,9 +35,11 @@ const buyIco = async(req,res) => {
                 // })
 
                 let investor_arr = ifIco[0].investors;
-                let k = 0;
+                console.log(ifIco[0].investors.length);
+
                 let flag_investor = 0
-                for(k=0; k<2; k++){
+                
+                for(let k=0; k<ifIco[0].investors.length ; k++){
                     if( investor_arr[k].investor_id === investor_id){
                         console.log('hai');
                         flag_investor = 1;
@@ -47,14 +48,58 @@ const buyIco = async(req,res) => {
                 }
 
                 if(flag_investor === 0){
+                    console.log('inside if');
+                    const updatedIco = await Ico.updateOne({
+                        id: ico_id
+                    },{
+                        $push: {
+                            investors: {
+                                investor_id: investor_id,
+                                qty: qty
+                            }
+                        }
+                    },
+                        {new: true}
+                    )
+
+                    const updateInvestor = await Investor.updateOne({
+                        id: investor_id
+                    },{
+                        $push: {
+                            icos: {
+                                ico_id: ico_id,
+                                qty: qty
+                            }
+                        }
+                    },
+                    {new: true})
+
+                    const totalAmount = ((ifIco[0].price)/ifIco[0].total_ico) * qty;
+
+                    const transaction = await Txn.create({
+                        txn_id:  uuidv4(),
+                        farmer_id: ifIco[0].farmer_id,
+                        investor_id: investor_id,
+                        ico_id: ico_id,
+                        amount: totalAmount,
+                        status: "Paid"
+                    })
+
+                    return res.status(200).json({
+                        code: 200,
+                        message: "new investor added",
+                        Txn: transaction.txn_id,
+                        error: false
+                    })
 
                 }else if(flag_investor === 1){
-                    const updateIco = await Ico.updateOne({
-                        id :ico_id
-                    },{
-                        sold_ico: sold_ico + qty,
-                        // ifIco[0].investors[k].qty: ifIco[0].investors[k].qty+qty
-                    })                    
+                    // const updateIco = await Ico.updateOne({
+                    //     id :ico_id
+                    // },{
+                    //     sold_ico: sold_ico + qty,
+                    //     // ifIco[0].investors[k].qty: ifIco[0].investors[k].qty+qty
+                    // })
+                    console.log('inside else if');                    
                 }
 
 
@@ -72,6 +117,9 @@ const buyIco = async(req,res) => {
                 error: true
             })
         }
+
+        return res.send('yo')
+
     }catch(error){
         console.log(error);
         res.status(500).json({
